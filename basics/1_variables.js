cf1d8a7e6c529b7c3562d226869008a19af46eef
Amikurmi <amitkurmiq19@gmail.com>
2023-12-29 11:13:35 +0530

    variables

---
+++ basics/1_variables.js
@@ -0,0 +1,15 @@
+const accountId=1233
+let accountName="amit kumar"
+var accountCity="bareilly"
+accountState="up"
+let name
+
+console.table([accountId,accountName,accountCity,accountState,name]);
+
+
+// accountId=11111
+accountCity="bly"
+accountName="amit"
+accountState="lmp"
+
+console.table([accountId,accountName,accountCity,accountState,name]);
