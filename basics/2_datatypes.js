3072419811f17f92028b0eff875436fa1e9d16a1
Amikurmi <amitkurmiq19@gmail.com>
2023-12-29 11:14:14 +0530

    dataType

---
+++ basics/2_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all JS code as newer version
+
+
+// alert(3+2)
+
+let name="amit"
+let age=20
+let isLogged=true
+let state =null;
+let unique=undefined
+
+console.table([name,age,isLogged,state,unique])
+
+
+//  number => 2 to power 53
+//  bigint
+//  string => ""
+//  boolean => true/false
+//  null => standalone value
+//  undefined => 
+//  symbol => unique
+
+//  object
+
+console.log(typeof undefined);  // undefined
+
+console.log(typeof null);    // object
