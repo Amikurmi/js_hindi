2c2ffd2d98df3dea710d9020d3c698f83f98a9fd
Amikurmi <amitkurmiq19@gmail.com>
2023-12-29 11:15:04 +0530

    conversion

---
+++ basics/3_ConversionOperation.js
@@ -0,0 +1,21 @@
+let score = 33
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof (valueInNumber));
+
+// "33"=> 33
+// "33abc"=> NaN
+// true=> 1; false=> 0
+
+let isLogged = 1
+
+let booleanisLoggedIn = Boolean(isLogged)
+console.log(booleanisLoggedIn);
+console.log(typeof booleanisLoggedIn);
+
+let someNumber = 23
+let StringNumber =String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber);;
