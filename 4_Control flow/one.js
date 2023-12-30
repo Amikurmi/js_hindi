aeff370f7ee75c908f245df7deb09681c1aac43a
Amikurmi <amitkurmiq19@gmail.com>
2023-12-30 15:00:36 +0530

    Control flow

---
+++ 4_Control flow/one.js
@@ -0,0 +1,100 @@
+// const temp =41
+
+// if(temp==41){
+//     console.log("temprature is 41");
+// }
+// else{
+//     console.log("Temprature is less than 50");
+// }
+
+// const score = 200
+// if(score>100){
+//     const power = "fly"
+//     console.log(`User Power : ${power}`);
+// }
+
+
+const balance =1000
+// if (balance > 500) console.log("test");
+// console.log("test2");
+// if (balance > 500) console.log("test"),console.log("test2");
+
+
+// if (balance < 500){
+//     console.log("less than ");
+// }else if(balance < 750){
+//     console.log("less than 750");
+// }else if (balance < 900) {
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+// }
+
+
+// const userLoggedIn = true
+// const debitCard = true
+// if (userLoggedIn && debitCard){
+//     console.log("allow to buy course");
+// }
+
+
+const month = 3
+switch(month){
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("February");
+        break;
+            
+    case 3:
+        console.log("March");
+        break;
+
+    case 4:
+        console.log("aprail");
+        break;
+    case 5:
+        console.log("May");
+         break;
+                
+     case 6:
+        console.log("June");
+        break;
+
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("Agust");
+        break;
+            
+    case 9:
+        console.log("September");
+        break;
+
+
+    case 10:
+        console.log("October");
+         break;
+    case 11:
+        console.log("November");
+        break;
+                
+    case 12:
+        console.log("December");
+        break;
+
+    default:
+        console.log("Month is not exists");
+        break;
+}
+                
+            
+                
+            
+ 
+            
+            
+            
+        
